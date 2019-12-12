67790155579361d2497587f763054ac21db91bae
Coder-B <1165457326@qq.com>
2019-12-12 19:37:02 +0800

    support key as input

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "alicloud" {
   version              = ">=1.56.0"
+  access_key           = var.access_key != "" ? var.access_key : null
+  secret_key           = var.secret_key != "" ? var.secret_key : null
   region               = var.region != "" ? var.region : null
   configuration_source = "terraform-will-modules/mq"
 }
--- variables.tf
+++ variables.tf
@@ -12,3 +12,13 @@ variable "region" {
   description = "The region used to launch this module resources."
   default     = ""
 }
+
+variable "access_key" {
+  description = "user access_key"
+  default     = ""
+}
+
+variable "secret_key" {
+  description = "user secret_key"
+  default     = ""
+}
